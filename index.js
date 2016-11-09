f44dbdc78ac70cc120f8d3faf2ccb4ae1a321d5b
abinocoe <abigailcoe@gmail.com>
2016-11-09 13:25:23 +0000

    styles

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app     = express();
+const port    = process.env.PORT || 3000;
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get("/*", (req, res) => res.sendFile(`${__dirname}/index.html`));
+
+app.listen(port, () => console.log(`App running on port ${port}`));
---
+++ src/js/controllers/main.js
@@ -0,0 +1,18 @@
+// angular
+//   .module('abigail')
+//   .controller('MainCtrl', MainCtrl);
+//
+// MainCtrl.$inject = ['$document'];
+// function MainCtrl($document) {
+//   // const vm = this;
+//   //
+//   // vm.toTheTop = function() {
+//   //     $document.scrollTopAnimated(0, 5000).then(function() {
+//   //       console.log('You just scrolled to the top!');
+//   //     });
+//   //   };
+//   // var portfolio = angular.element(document.getElementById('section-3'));
+//   // vm.toPortfolio = function() {
+//   //   $document.scrollToElementAnimated(portfolio);
+//   // };
+// }
